19696ef21df4c37f5ad647182b6d6db1ea558097
Nick Malcolm <nick@revert.io>
2015-05-28 10:47:48 +1200

    Job Design nitpicks. The tests pass ;)

--- lib/salesforce-bulk-query/job.rb
+++ lib/salesforce-bulk-query/job.rb
@@ -1,19 +1,26 @@
 module SalesforceBulkQuery
   class Job
 
+    # Public: the id of the Job. Assigned when we `create` the Job
+    # on SalesForce.
     attr_accessor :id
+    # Public: batch_id is assigned when SalesForce begins executing the Job.
+    attr_accessor :batch_id
+    # Public: The results of the Job.
+    attr_accessor :results
+
 
     def initialize(client, object_type)
       @client = client
-
-      create_job(object_type)
+      @object_type = object_type
+      create
     end
 
-    def create_job(object_type)
+    def create
       xml =  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
       xml << "<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
       xml << "<operation>query</operation>"
-      xml << "<object>#{object_type}</object>"
+      xml << "<object>#{@object_type}</object>"
       xml << "<concurrencyMode>Parallel</concurrencyMode>"
       xml << "<contentType>XML</contentType>"
       xml << "</jobInfo>"
@@ -24,42 +31,63 @@ module SalesforceBulkQuery
     end
 
     def execute(query)
+      require_id
       response = @client.post("/job/#{id}/batch", query, headers: {"Content-Type" => "application/xml; charset=UTF-8"})
 
       @batch_id = response["batchInfo"]["id"]
 
+      wait_for_results
+    end
+
+    def wait_for_results(sleep_time=2)
       loop do
-        if job_completed
-          return results
+        if completed?
+          return get_results
         else
-          sleep(2)
+          sleep sleep_time
         end
       end
     end
 
-    def job_completed
-      response = @client.get("/job/#{id}/batch/#{@batch_id}")
+    def completed?
+      require_id && require_batch_id
+
+      response = @client.get("/job/#{id}/batch/#{batch_id}")
 
       case response["batchInfo"]["state"]
-        when "Completed"
-          return true
-        when "Failed"
-          raise JobError.new response["batchInfo"]["stateMessage"]
-        else
-          return false
+      when "Completed"
+        return true
+      when "Failed"
+        raise JobError.new response["batchInfo"]["stateMessage"]
+      else
+        return false
       end
     end
 
-    def results
-      response = @client.get("/job/#{id}/batch/#{@batch_id}/result")
+    # Public: Will GET and parse results from a completed Job.
+    # Assigns the results to the `results` attribute.
+    # Returns the results.
+    def get_results
+      require_id && require_batch_id
+
+      response = @client.get("/job/#{id}/batch/#{batch_id}/result")
 
       results = []
       results << response["result_list"]["result"]
 
-      results.flatten.map do |result_id|
-        JobResult.new(@client, id, @batch_id, result_id)
+      @results = results.flatten.map do |result_id|
+        JobResult.new(@client, id, batch_id, result_id)
       end
     end
+
+    private
+      def require_id
+        raise ArgumentError, "`id` required. Have you run `create`?" unless id
+      end
+
+      def require_batch_id
+        raise ArgumentError, "`batch_id` required. Have you run `execute`?" unless batch_id
+      end
 
   end
 
